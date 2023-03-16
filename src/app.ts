3990d82bccbbf96681da730923cfb3695d2c9a4b
unknown <jonashakon01@gmail.com>
2023-03-16 19:18:50 +0000

    Byrja að implementa validation með Tokens inni í app.ts

--- src/app.ts
+++ src/app.ts
@@ -3,7 +3,30 @@ import express, { NextFunction, Request, Response } from 'express';
 import { cors } from './lib/cors.js';
 import { router } from './routes/api.js';
 
+import passport from 'passport';
+import { Strategy, ExtractJwt } from 'passport-jwt';
+import jwt from 'jsonwebtoken';
+
+import {
+  comparePasswords,
+  findByUsername,
+  findById,
+} from './users.js';
+//TODO: Implement user.ts
+
 dotenv.config();
+
+const {
+  PORT: port = 3000,
+  JWT_SECRET: jwtSecret,
+  TOKEN_LIFETIME: tokenLifetime = 20,
+  DATABASE_URL: databaseUrl,
+} = process.env;
+
+if (!jwtSecret || !databaseUrl) {
+  console.error('Vantar .env gildi');
+  process.exit(1);
+}
 
 const app = express();
 
@@ -12,7 +35,81 @@ app.use(express.json());
 app.use(cors);
 app.use(router);
 
-const port = process.env.PORT || 3000;
+const jwtOptions = {
+  jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+  secretOrKey: jwtSecret,
+};
+
+async function strat(data : any, next : NextFunction) {
+  // fáum id gegnum data sem geymt er í token
+  const user = await findById(data.id);
+
+  if (user) {
+    next(null, user : user);
+  } else {
+    next(null, false);
+  }
+}
+
+passport.use(new Strategy(jwtOptions, strat));
+
+app.use(passport.initialize());
+
+app.get('/', (req, res) => {
+  res.json({
+    login: '/login',
+    admin: '/admin',
+  });
+});
+
+app.post('/login', async (req, res) => {
+  const { username, password = '' } = req.body;
+
+  const user = await findByUsername(username);
+
+  if (!user) {
+    return res.status(401).json({ error: 'No such user' });
+  }
+
+  const passwordIsCorrect = await comparePasswords(password, user.password);
+
+  if (passwordIsCorrect) {
+    const payload = { id: user.id };
+    const tokenOptions = { expiresIn: tokenLifetime };
+    const token = jwt.sign(payload, jwtOptions.secretOrKey, tokenOptions);
+    return res.json({ token });
+  }
+
+  return res.status(401).json({ error: 'Invalid password' });
+});
+
+function requireAuthentication(req, res, next) {
+  return passport.authenticate(
+    'jwt',
+    { session: false },
+    (err, user, info) => {
+      if (err) {
+        return next(err);
+      }
+
+      if (!user) {
+        const error = info.name === 'TokenExpiredError'
+          ? 'expired token' : 'invalid token';
+
+        return res.status(401).json({ error });
+      }
+
+      // Látum notanda vera aðgengilegan í rest af middlewares
+      req.user = user;
+      return next();
+    },
+  )(req, res, next);
+}
+
+app.get('/admin', requireAuthentication, (req, res) => {
+  res.json({ data: 'top secret' });
+});
+
 
 app.listen(port, () => {
   console.log(`Server running at http://localhost:${port}/`);
